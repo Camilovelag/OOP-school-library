78a71c67e4d6dc6578574e565305d24596915007
Camilovelag <camilo.vela.galindo@gmail.com>
2022-12-20 17:52:33 -0500

    Setup rubocop, bundler, add person class

---
+++ person.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# class Person
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private :of_age?
+end
+
+mat = Person.new(12, 'Mat', parent_permission: false)
+puts mat.inspect
+puts mat.can_use_services?
+puts mat.of_age?
