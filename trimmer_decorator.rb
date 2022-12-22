ed40c79f850c82c730aecd49737606ce23263520
Camilovelag <camilo.vela.galindo@gmail.com>
2022-12-21 19:50:33 -0500

    Add changes

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,6 +3,6 @@ require './decorator'
 # class trimmer_decorator
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name.length > 10 ? @nameable[0..9] : @nameable
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
